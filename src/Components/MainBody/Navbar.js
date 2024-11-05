01e027416a35aaccf92d6a3cd155e48006f1f9be
Shivang Verma <vermashivang777@gmail.com>
2024-11-06 01:34:03 +0530

    LogIn, SignUp and implemented route reactor dom frontend parts

--- src/Components/MainBody/Navbar.js
+++ src/Components/MainBody/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
-const isLoggedIn = true;
+import { Link } from 'react-router-dom';
+const isLoggedIn = false;
 const Navbar = () => {
   return (
-    <div className='h-16 flex items-center rounded-tl-xl bg-white rounded-tr-xl'>
-        <div className='w-1/3'>
+    <div className='h-16 flex items-center rounded-tl-xl rounded-tr-xl'>
+        <div>
             <a href='/' className='m-5'>MUSIC</a>
             <a href='/' className='m-5'>PODCASTS</a>
+            <a href='/' className='m-5'>LIVE</a>
         </div>
-        <div className='w-1/3'>
+        <div className='w-1/2 ml-10'>
             <form>
                 <input name='search' placeholder='Search Here...' type='text' className='shadow-lg mr-1 h-[5vh] w-[40vh] rounded-tl-xl rounded-bl-xl p-2'></input>
                 <button type='submit' className='button h-[5vh] w-[10vh] rounded-tr-xl rounded-br-xl'>Search</button>
@@ -22,8 +24,8 @@ const Navbar = () => {
                 </>
                 :
                 <>
-                <a href='/' className='m-3'>SignUp</a>
-                <a href='/' className='m-3'>Login</a>
+                <Link to='/signup' className='m-3'>SignUp</Link>
+                <Link to='/login' className='m-3'>Login</Link>
                 </>
             }
         </div>
--- src/screenDiv/Main.js
+++ src/screenDiv/Main.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Navbar from '../Components/MainBody/Navbar'
+import {Routes, Route} from "react-router-dom"
+import LoginForm from '../Components/Assets/LoginForm'
+import SignUp from '../Components/Assets/SignUp'
+import Home from '../Components/MainBody/Home'
+import ErrorPage from '../Components/Assets/ErrorPage'
 
 const Main = () => {
   return (
-    <div className='h-screen w-4/5 m-1 rounded-xl'>
+    <div className='w-4/5'>
+    <div className='m-1 rounded-xl'>
       <Navbar/>
+    </div>
+    <div>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<LoginForm/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
+    </div>
     </div>
   )
 }
