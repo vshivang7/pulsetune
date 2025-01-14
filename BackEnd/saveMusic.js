6fa7b170ce41ef56572496fed13f5b626460d8a5
Samit <samitsonkar0@gmail.com>
2025-01-14 22:29:36 +0530

    saveMusic added

---
+++ BackEnd/saveMusic.js
@@ -0,0 +1,27 @@
+const connectdb = require('./connectDb');
+const Music = require('./Models/musicSchema');
+const fetchData = async () => {
+    let response = await fetch(`https://v1.nocodeapi.com/vshivang/spotify/dTkOvSBZSnjjGDpb/search?q=sunny&type=track`)
+    let data = await response.json();
+    return data;
+}
+
+const saveMusic = async() => {
+    await connectdb();
+    let datas = await fetchData();
+    datas = datas.tracks.items;
+    // console.log(datas);
+    for(let data of datas) {
+        if(data.preview_url==null) continue;
+        let m = new Music({
+            image: data.album.images[0].url,
+            song_name: data.name,
+            artist: data.artists[0].name,
+            url: data.preview_url,
+        })
+        console.log(m);
+        await m.save();
+    }
+}
+
+saveMusic()
