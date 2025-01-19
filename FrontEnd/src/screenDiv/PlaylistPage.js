88dec3d47c903459fa69042ac263dbd0cf708e3e
Shivang Verma <vermashivang777@gmail.com>
2025-01-20 00:29:01 +0530

    playlist delete Frontend

--- FrontEnd/src/screenDiv/PlaylistPage.js
+++ FrontEnd/src/screenDiv/PlaylistPage.js
@@ -31,7 +31,7 @@ const PlaylistPage = ({user, setUser}) => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-6 m-8">
       {user.playlists.map((playlist) => (
         <div key={playlist.id}>
-          <PlaylistCard playlist={playlist} />
+          <PlaylistCard playlist={playlist} setUser={setUser} />
         </div>
       ))}
     </div>
