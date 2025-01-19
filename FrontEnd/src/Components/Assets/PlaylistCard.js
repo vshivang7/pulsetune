4d974e00fa205ad9fdf78425515b6d8e0ef5198a
Shivang Verma <vermashivang777@gmail.com>
2025-01-20 00:17:18 +0530

    playlist delete frontend

--- FrontEnd/src/Components/Assets/PlaylistCard.js
+++ FrontEnd/src/Components/Assets/PlaylistCard.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const PlaylistCard = ({playlist}) => {
+const PlaylistCard = ({playlist, setUser}) => {
+  const handleDelete = async () => {
+    try {
+      let res = await fetch(`http://localhost:8080/playlist/${playlist._id}`, {
+        method: 'DELETE',
+        credentials: 'include',
+      });
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+
+      let data = await res.json();
+      if (data) setUser(data);
+    } catch (error) {
+    }
+  }
+
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
   <div className="flex justify-end px-4 pt-4">
@@ -74,12 +91,12 @@ const PlaylistCard = ({playlist}) => {
       >
         View Playlist
       </Link>
-      <a
-        href="#"
+      <button
+        onClick={handleDelete}
         className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
       >
         Delete
-      </a>
+      </button>
     </div>
   </div>
 </div>
