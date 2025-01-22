82e4e12df6943f2819674fcb7b211855b562d124
Shivang Verma <vermashivang777@gmail.com>
2025-01-23 01:08:54 +0530

    playlist opacity change

--- FrontEnd/src/Components/Assets/PlaylistCard.js
+++ FrontEnd/src/Components/Assets/PlaylistCard.js
@@ -19,36 +19,30 @@ const PlaylistCard = ({playlist, setUser}) => {
     }
   }
   return (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-  <div className="flex justify-end px-4 pt-4"></div>
-  <div className="flex flex-col items-center pb-10">
-    <img
-      className="w-24 h-24 mb-3 rounded-full shadow-lg"
-      src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      alt="Bonnie Green"
-    />
-    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-      {playlist.name.charAt(0).toUpperCase() + playlist.name.slice(1)}
-    </h5>
-    <span className="text-sm text-gray-500 dark:text-gray-400">
-      Songs : {playlist.list.length}
-    </span>
-    <div className="flex mt-4 md:mt-6">
-      <Link
-        to={`/playlist/${playlist._id}`}
-        className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        View Playlist
-      </Link>
-      <button
-        onClick={handleDelete}
-        className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-      >
-        Delete
-      </button>
+  <div className="opacity-90 flex flex-col justify-evenly w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    {/* <div className="flex justify-end px-4 pt-4"></div> */}
+    <div className="flex flex-col items-center py-5">
+      <img className="w-24 h-24 mb-3 rounded-full shadow-lg"
+        src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Bonnie Green"/>
+      <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+        {playlist.name.charAt(0).toUpperCase() + playlist.name.slice(1)}
+      </h5>
+      <span className="text-sm text-gray-500 dark:text-gray-400">
+        Songs : {playlist.list.length}
+      </span>
+      <div className="flex mt-4 md:mt-6">
+        <Link
+          to={`/playlist/${playlist._id}`}
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          View Playlist
+        </Link>
+        <button onClick={handleDelete} className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+          Delete
+        </button>
+      </div>
     </div>
   </div>
-</div>
   )
 }
 
