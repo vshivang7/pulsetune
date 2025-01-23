fa0cec89c9138ba8bb96d087411a6ad4f5d0cd2d
Shivang Verma <vermashivang777@gmail.com>
2025-01-23 15:19:44 +0530

    Search Functionality implemented successfully

--- FrontEnd/src/Components/MainBody/Home.js
+++ FrontEnd/src/Components/MainBody/Home.js
@@ -8,18 +8,25 @@ const Home = ({search, user, setUser, musics}) => {
 
 useEffect(() => {
   const filtered = musics.filter((music) => 
-  music.song_name.includes(search)
+    music.song_name.toLowerCase().includes(search.toLowerCase()) || music.artist.toLowerCase().includes(search.toLowerCase())
   );
   setFilteredMusic(filtered);
-}, [search]);
+}, [search, musics]);
+
+// let curr=musics;
   return (
     <div className='flex flex-wrap w-full justify-evenly mt-5 mb-64'>
     {
-      filteredMusic.map((music) => {
+      search ? (filteredMusic.length>0 ? 
+        filteredMusic.map((music) => {
+          return (
+            <div className='w-1/4 h-fit m-2' key={music._id}><Card setUser={setUser} isPlaying = {isPlaying} setIsPlaying = {setIsPlaying} audioRef = {audioRef} image = {music.image} song_name = {music.song_name} artist = {music.artist} url = {music.url} user = {user} id = {music._id}/></div>) })
+          : (<div className='w-full h-96 flex justify-center items-center'><h1 className='text-2xl'>No Song Found</h1></div>)
+          )
+      : musics.map((music) => {
         return (
-          <div className='w-1/4 h-fit m-2' key={music._id}><Card setUser={setUser} isPlaying = {isPlaying} setIsPlaying = {setIsPlaying} audioRef = {audioRef} image = {music.image} song_name = {music.song_name} artist = {music.artist} url = {music.url} user = {user} id = {music._id}/></div>
-        )
-      })
+          <div className='w-1/4 h-fit m-2' key={music._id}><Card setUser={setUser} isPlaying = {isPlaying} setIsPlaying = {setIsPlaying} audioRef = {audioRef} image = {music.image} song_name = {music.song_name} artist = {music.artist} url = {music.url} user = {user} id = {music._id}/></div>) })
+      
     }
     </div>
   )
